2bffebb632fbfec6b3ed875c170b88033fb16095
sirwhod <rodrigo.brandao98@gmail.com>
2023-01-17 22:22:36 -0300

    Headr & Layout/Página:Home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,24 @@
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logoIgnite from '../../assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
-  return <h1>header</h1>
+  return (
+    <HeaderContainer>
+      <img
+        src={logoIgnite}
+        alt="Dois triângulos um em cima do outro na cor verde"
+      />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
